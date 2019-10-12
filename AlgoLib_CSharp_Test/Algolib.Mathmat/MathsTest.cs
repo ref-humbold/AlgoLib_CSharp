f3f7cd96340cfdc5a1010fa0b833d0f0cc8c5fe5
R. Kaleta <rafkaleta@onet.eu>
2019-10-12 21:27:53 +0200

    New tests for LCM

--- AlgoLib_CSharp_Test/Algolib.Mathmat/MathsTest.cs
+++ AlgoLib_CSharp_Test/Algolib.Mathmat/MathsTest.cs
@@ -59,5 +59,60 @@ namespace Algolib.Mathmat
 
             Assert.AreEqual(number1, result);
         }
+
+        [Test]
+        public void LCM_WhenNumbersAreComposite_ThenReturnsLCM()
+            {
+                long number1 = 161;
+                long number2 = 46;
+
+                long result = Maths.LCM(number1, number2);
+
+                Assert.AreEqual(322, result);
+            }
+
+        [Test]
+        public void LCM_WhenNumbersArePrime_ThenReturnsProduct()
+            {
+                long number1 = 127;
+                long number2 = 41;
+
+                long result = Maths.LCM(number1, number2);
+
+                Assert.AreEqual(5207, result);
+            }
+
+        [Test]
+        public void LCM_WhenNumbersAreMutuallyPrime_ThenReturnsProduct()
+            {
+                long number1 = 119;
+                long number2 = 57;
+
+                long result = Maths.LCM(number1, number2);
+
+                Assert.AreEqual(6783, result);
+            }
+
+        [Test]
+        public void LCM_WhenNumber1IsMultipleOfNumber2_ThenReturnsNumber1()
+            {
+                long number1 = 272;
+                long number2 = 34;
+
+                long result = Maths.LCM(number1, number2);
+
+                Assert.AreEqual(number1, result);
+            }
+
+        [Test]
+        public void LCM_WhenOneOfNumbersIsZero_ThenReturnsZero()
+            {
+                long number1 = 96;
+                long number2 = 0;
+
+                long result = Maths.LCM(number1, number2);
+
+                Assert.AreEqual(0, result);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AlgoLib_CSharp
 ALGOrithms LIBrary - C# version
+
+Library of various algorithms and data structures.
