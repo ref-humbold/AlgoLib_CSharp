5459bc5a05821069638278b7307da25732c088f3
R. Kaleta <rafkaleta@onet.eu>
2020-07-18 16:18:38 +0200

    Graph cutting algorithm

---
+++ AlgoLib_CSharp/Algolib.Graphs.Algorithms/Cutting.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algolib.Graphs.Algorithms
+{
+    public sealed class Cutting
+    {
+        /// <summary>Finds an edge cut of given graph.</summary>
+        /// <param name="graph">an undirected graph</param>
+        /// <returns>edges in the edge cut</returns>
+        public static IEnumerable<Edge<V>> FindEdgeCut<V, VP, EP>(IUndirectedGraph<V, VP, EP> graph)
+        {
+            CuttingStrategy<V> strategy = new CuttingStrategy<V>();
+
+            Searching.DfsRecursive(graph, strategy, graph.Vertices);
+            return graph.Vertices
+                    .Where(vertex => strategy.HasBridge(vertex))
+                    .Select(vertex => graph.GetEdge(vertex, strategy.dfsParents[vertex]));
+        }
+
+        /// <summary>Finds a vertex cut of given graph.</summary>
+        /// <param name="graph">an undirected graph</param>
+        /// <returns>vertices in the vertex cut</returns>
+        public static IEnumerable<V> FindVertexCut<V, VP, EP>(IUndirectedGraph<V, VP, EP> graph)
+        {
+            CuttingStrategy<V> strategy = new CuttingStrategy<V>();
+
+            Searching.DfsRecursive(graph, strategy, graph.Vertices);
+            return graph.Vertices.Where(vertex => strategy.IsSeparator(vertex));
+        }
+
+        private class CuttingStrategy<V> : IDfsStrategy<V>
+        {
+            internal readonly Dictionary<V, V> dfsParents = new Dictionary<V, V>();
+            internal readonly Dictionary<V, List<V>> dfsChildren = new Dictionary<V, List<V>>();
+            internal readonly Dictionary<V, int> dfsDepths = new Dictionary<V, int>();
+            internal readonly Dictionary<V, int> lowValues = new Dictionary<V, int>();
+            private int depth = 0;
+
+            public void ForRoot(V root)
+            {
+            }
+
+            public void OnEntry(V vertex)
+            {
+                dfsDepths[vertex] = depth;
+                lowValues[vertex] = depth;
+                dfsChildren.TryAdd(vertex, new List<V>());
+                ++depth;
+            }
+
+            public void OnNextVertex(V vertex, V neighbour)
+            {
+                dfsParents[neighbour] = vertex;
+                dfsChildren[vertex].Add(neighbour);
+            }
+
+            public void OnExit(V vertex)
+            {
+                int? childLowValue = dfsChildren[vertex].Min(child => (int?)lowValues[child]);
+                int minimalLowValue = childLowValue ?? int.MaxValue;
+
+                lowValues[vertex] = Math.Min(lowValues[vertex], minimalLowValue);
+                --depth;
+            }
+
+            public void OnEdgeToVisited(V vertex, V neighbour)
+            {
+                if(!neighbour.Equals(dfsParents[vertex]))
+                    lowValues[vertex] = Math.Min(lowValues[vertex], dfsDepths[neighbour]);
+            }
+
+            internal bool HasBridge(V vertex)
+            {
+                return !IsDfsRoot(vertex) && lowValues[vertex] == dfsDepths[vertex];
+            }
+
+            internal bool IsSeparator(V vertex) =>
+                IsDfsRoot(vertex) ? dfsChildren[vertex].Count > 1
+                                  : dfsChildren[vertex].Any(
+                                          child => lowValues[child] >= dfsDepths[vertex]);
+
+            internal bool IsDfsRoot(V vertex) => dfsDepths[vertex] == 0;
+        }
+    }
+}
---
+++ AlgoLib_CSharp_Test/Algolib.Graphs.Algorithms/CuttingTests.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algolib.Graphs.Algorithms
+{
+    [TestFixture]
+    public class CuttingTests
+    {
+        [Test]
+        public void FindEdgeCut_WhenPresentBridges_ThenBridges()
+        {
+            // given
+            UndirectedSimpleGraph<int, object, object> graph =
+                new UndirectedSimpleGraph<int, object, object>(Enumerable.Range(0, 12));
+            graph.AddEdgeBetween(0, 1);
+            graph.AddEdgeBetween(0, 2);
+            graph.AddEdgeBetween(0, 7);
+            graph.AddEdgeBetween(1, 2);
+            graph.AddEdgeBetween(1, 3);
+            graph.AddEdgeBetween(1, 4);
+            graph.AddEdgeBetween(3, 5);
+            graph.AddEdgeBetween(4, 5);
+            graph.AddEdgeBetween(5, 6);
+            graph.AddEdgeBetween(7, 8);
+            graph.AddEdgeBetween(7, 9);
+            graph.AddEdgeBetween(7, 11);
+            graph.AddEdgeBetween(8, 9);
+            graph.AddEdgeBetween(9, 10);
+            graph.AddEdgeBetween(9, 11);
+            graph.AddEdgeBetween(10, 11);
+            // when
+            IEnumerable<Edge<int>> result = Cutting.FindEdgeCut(graph);
+            // then
+            CollectionAssert.AreEquivalent(
+                new List<Edge<int>>() { graph.GetEdge(0, 7), graph.GetEdge(5, 6) }, result);
+        }
+
+        [Test]
+        public void FindEdgeCut_WhenNoBridges_ThenEmptyList()
+        {
+            // given
+            UndirectedSimpleGraph<int, object, object> graph =
+                new UndirectedSimpleGraph<int, object, object>(Enumerable.Range(0, 6));
+            graph.AddEdgeBetween(0, 1);
+            graph.AddEdgeBetween(0, 2);
+            graph.AddEdgeBetween(1, 2);
+            graph.AddEdgeBetween(1, 3);
+            graph.AddEdgeBetween(1, 4);
+            graph.AddEdgeBetween(3, 5);
+            graph.AddEdgeBetween(4, 5);
+            // when
+            IEnumerable<Edge<int>> result = Cutting.FindEdgeCut(graph);
+            // then
+            CollectionAssert.IsEmpty(result);
+        }
+
+        [Test]
+        public void FindVertexCut_WhenPresentSeparators_ThenSeparators()
+        {
+            // given
+            UndirectedSimpleGraph<int, object, object> graph =
+                new UndirectedSimpleGraph<int, object, object>(Enumerable.Range(0, 12));
+            graph.AddEdgeBetween(0, 1);
+            graph.AddEdgeBetween(0, 2);
+            graph.AddEdgeBetween(0, 7);
+            graph.AddEdgeBetween(1, 2);
+            graph.AddEdgeBetween(1, 3);
+            graph.AddEdgeBetween(1, 4);
+            graph.AddEdgeBetween(3, 5);
+            graph.AddEdgeBetween(4, 5);
+            graph.AddEdgeBetween(5, 6);
+            graph.AddEdgeBetween(7, 8);
+            graph.AddEdgeBetween(7, 9);
+            graph.AddEdgeBetween(7, 11);
+            graph.AddEdgeBetween(8, 9);
+            graph.AddEdgeBetween(9, 10);
+            graph.AddEdgeBetween(9, 11);
+            graph.AddEdgeBetween(10, 11);
+            // when
+            IEnumerable<int> result = Cutting.FindVertexCut(graph);
+            // then
+            CollectionAssert.AreEquivalent(new List<int>() { 0, 1, 5, 7 }, result);
+        }
+
+        [Test]
+        public void FindVertexCut_WhenNoSeparators_ThenEmptyList()
+        {
+            // given
+            UndirectedSimpleGraph<int, object, object> graph =
+                new UndirectedSimpleGraph<int, object, object>(Enumerable.Range(0, 6));
+            graph.AddEdgeBetween(0, 1);
+            graph.AddEdgeBetween(0, 2);
+            graph.AddEdgeBetween(1, 2);
+            graph.AddEdgeBetween(1, 3);
+            graph.AddEdgeBetween(1, 4);
+            graph.AddEdgeBetween(2, 3);
+            graph.AddEdgeBetween(3, 5);
+            graph.AddEdgeBetween(4, 5);
+            // when
+            IEnumerable<int> result = Cutting.FindVertexCut(graph);
+            // then
+            CollectionAssert.IsEmpty(result);
+        }
+    }
+}
