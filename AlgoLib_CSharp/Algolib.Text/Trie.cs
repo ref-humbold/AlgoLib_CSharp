c40c6680d52e5dab2636de496aa6b66118766b7f
R. Kaleta <rafkaleta@onet.eu>
2021-06-04 21:51:23 +0200

    Trie structure

---
+++ AlgoLib_CSharp/Algolib.Text/Trie.cs
@@ -0,0 +1,114 @@
+﻿// Structure of trie tree
+using System.Collections.Generic;
+
+namespace Algolib.Text
+{
+    public class Trie
+    {
+        private TrieNode tree = new TrieNode();
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public Trie() => Count = 0;
+
+        public Trie(IEnumerable<string> texts) : this()
+        {
+            foreach(string text in texts)
+                Add(text);
+        }
+
+        public void Add(string text)
+        {
+            TrieNode node = tree;
+
+            foreach(char character in text)
+            {
+                node.Add(character, new TrieNode());
+                node = node[character];
+            }
+
+            if(!node.Terminus)
+            {
+                ++Count;
+                node.Terminus = true;
+            }
+        }
+
+        public void Clear()
+        {
+            tree = new TrieNode();
+            Count = 0;
+        }
+
+        public bool Contains(string text)
+        {
+            TrieNode node = tree;
+
+            foreach(char character in text)
+            {
+                node = node[character];
+
+                if(node == null)
+                    return false;
+            }
+
+            return node.Terminus;
+        }
+
+        public void Remove(string text)
+        {
+            if(text.Length > 0)
+                removeNode(text, tree, 0);
+        }
+
+        private bool removeNode(string text, TrieNode node, int i)
+        {
+            if(i == text.Length && node.Terminus)
+            {
+                --Count;
+                node.Terminus = false;
+            }
+            else if(i < text.Length)
+            {
+                TrieNode nextNode = node[text[i]];
+
+                if(nextNode != null && removeNode(text, nextNode, i + 1))
+                    node.Remove(text[i]);
+            }
+
+            return !node.Terminus && node.Count == 0;
+        }
+
+        private class TrieNode
+        {
+            private readonly Dictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+
+            internal int Count => children.Count;
+
+            internal bool Terminus
+            {
+                get;
+                set;
+            }
+
+            internal TrieNode() => Terminus = false;
+
+            internal TrieNode this[char c]
+            {
+                get
+                {
+                    children.TryGetValue(c, out TrieNode node);
+                    return node;
+                }
+            }
+
+            internal void Add(char c, TrieNode node) => children.TryAdd(c, node);
+
+            internal void Remove(char c) => children.Remove(c);
+        }
+    }
+}
---
+++ AlgoLib_CSharp_Test/Algolib.Text/TrieTests.cs
@@ -0,0 +1,145 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace Algolib.Text
+{
+    [TestFixture]
+    public class TrieTest
+    {
+        private readonly List<string> texts = new List<string> { "abcd", "ab", "xyz" };
+        private Trie testObject;
+
+        [SetUp]
+        public void SetUp() => testObject = new Trie(texts);
+
+        [Test]
+        public void Count_WhenEmpty_ThenZero()
+        {
+            // given
+            testObject = new Trie();
+            // when
+            int result = testObject.Count;
+            // then
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void Count_WhenNotEmpty_ThenNumberOfTexts()
+        {
+            // when
+            int result = testObject.Count;
+            // then
+            result.Should().Be(texts.Count);
+        }
+
+        [Test]
+        public void Add_WhenPresent_ThenNothingChanged()
+        {
+            // given
+            string text = "abcd";
+            // when
+            testObject.Add(text);
+            // then
+            testObject.Contains(text).Should().BeTrue();
+            testObject.Count.Should().Be(texts.Count);
+        }
+
+        [Test]
+        public void Add_WhenAbsent_ThenAdded()
+        {
+            // given
+            string text = "abxx";
+            // when
+            testObject.Add(text);
+            // then
+            testObject.Contains(text).Should().BeTrue();
+            testObject.Count.Should().Be(texts.Count + 1);
+        }
+
+        [Test]
+        public void Add_WhenAbsentPrefix_ThenAdded()
+        {
+            // given
+            string text = "xy";
+            // when
+            testObject.Add(text);
+            // then
+            testObject.Contains(text).Should().BeTrue();
+            testObject.Count.Should().Be(texts.Count + 1);
+        }
+
+        [Test]
+        public void Clear_WhenNotEmpty_ThenEmpty()
+        {
+            // when
+            testObject.Clear();
+            // then
+            testObject.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void Contains_WhenPresent_ThenTrue()
+        {
+            // when
+            bool result = testObject.Contains("abcd");
+            // then
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Contains_WhenAbsent_ThenFalse()
+        {
+            // when
+            bool result = testObject.Contains("abxx");
+            // then
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Contains_WhenAbsentPrefix_ThenFalse()
+        {
+            // when
+            bool result = testObject.Contains("xy");
+            // then
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Remove_WhenPresent_ThenRemoved()
+        {
+            // given
+            string text = "abcd";
+            // when
+            testObject.Remove(text);
+            // then
+            testObject.Contains(text).Should().BeFalse();
+            testObject.Count.Should().Be(texts.Count - 1);
+        }
+
+        [Test]
+        public void Remove_WhenAbsent_ThenNothingChanged()
+        {
+            // given
+            string text = "abxx";
+            // when
+            testObject.Remove(text);
+            // then
+            testObject.Contains(text).Should().BeFalse();
+            testObject.Count.Should().Be(texts.Count);
+        }
+
+        [Test]
+        public void Remove_WhenAbsentPrefix_ThenNothingChanged()
+        {
+            // given
+            string text = "xy";
+            // when
+            testObject.Remove(text);
+            // then
+            testObject.Contains("xyz").Should().BeTrue();
+            testObject.Contains(text).Should().BeFalse();
+            testObject.Count.Should().Be(texts.Count);
+        }
+    }
+}
