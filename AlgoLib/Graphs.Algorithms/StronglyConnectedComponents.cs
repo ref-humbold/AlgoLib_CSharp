fa5957592a6f6b9fc38d46e7f84e835e7d9a827a
R. Kaleta <rafkaleta@onet.eu>
2021-10-24 22:14:11 +0200

    Strongly connected components

---
+++ AlgoLib/Graphs.Algorithms/StronglyConnectedComponents.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algolib.Graphs.Algorithms
+{
+    public static class StronglyConnectedComponents
+    {
+        /// <summary>Finds strongly connected components in given directed graph.</summary>
+        /// <param name="graph">A directed graph</param>
+        /// <returns>List of vertices in strongly connected components</returns>
+        public static List<HashSet<Vertex<TVertexId>>> FindScc<TVertexId, TVertexProperty, TEdgeProperty>(
+                this IDirectedGraph<TVertexId, TVertexProperty, TEdgeProperty> graph)
+        {
+            PostOrderStrategy<TVertexId> postOrderStrategy = new PostOrderStrategy<TVertexId>();
+
+            Searching.DfsRecursive(graph, postOrderStrategy, graph.Vertices);
+
+            List<Vertex<TVertexId>> vertices = postOrderStrategy.postTimes
+                                                                .OrderByDescending(kv => kv.Value)
+                                                                .Select(kv => kv.Key)
+                                                                .ToList();
+            IDirectedGraph<TVertexId, TVertexProperty, TEdgeProperty> reversedGraph = graph.ReversedCopy();
+            SCCStrategy<TVertexId> sccStrategy = new SCCStrategy<TVertexId>();
+
+            Searching.DfsRecursive(reversedGraph, sccStrategy, vertices);
+            return sccStrategy.components;
+        }
+
+        private class PostOrderStrategy<TVertexId> : IDfsStrategy<TVertexId>
+        {
+            internal readonly Dictionary<Vertex<TVertexId>, int> postTimes =
+                new Dictionary<Vertex<TVertexId>, int>();
+
+            private int timer = 0;
+
+            public void ForRoot(Vertex<TVertexId> root)
+            {
+            }
+
+            public void OnEntry(Vertex<TVertexId> vertex)
+            {
+            }
+
+            public void OnNextVertex(Vertex<TVertexId> vertex, Vertex<TVertexId> neighbour)
+            {
+            }
+
+            public void OnExit(Vertex<TVertexId> vertex)
+            {
+                postTimes[vertex] = timer;
+                ++timer;
+            }
+
+            public void OnEdgeToVisited(Vertex<TVertexId> vertex, Vertex<TVertexId> neighbour)
+            {
+            }
+        }
+
+        private class SCCStrategy<TVertexId> : IDfsStrategy<TVertexId>
+        {
+            internal readonly List<HashSet<Vertex<TVertexId>>> components =
+                new List<HashSet<Vertex<TVertexId>>>();
+
+            public void ForRoot(Vertex<TVertexId> root) =>
+                components.Add(new HashSet<Vertex<TVertexId>>());
+
+            public void OnEntry(Vertex<TVertexId> vertex) => components[^1].Add(vertex);
+
+            public void OnNextVertex(Vertex<TVertexId> vertex, Vertex<TVertexId> neighbour)
+            {
+            }
+
+            public void OnExit(Vertex<TVertexId> vertex)
+            {
+            }
+
+            public void OnEdgeToVisited(Vertex<TVertexId> vertex, Vertex<TVertexId> neighbour)
+            {
+            }
+        }
+    }
+}
