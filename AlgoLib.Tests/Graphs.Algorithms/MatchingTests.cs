e73852a53d404a20bb8ebb26aea1a6add2929e18
R. Kaleta <rafkaleta@onet.eu>
2021-11-09 22:30:15 +0100

    Matching algorithm

---
+++ AlgoLib.Tests/Graphs.Algorithms/MatchingTests.cs
@@ -0,0 +1,70 @@
+﻿// Tests: Hopcroft-Karp algorithm for matching in bipartite graph
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AlgoLib.Graphs.Algorithms
+{
+    [TestFixture()]
+    public class MatchingTests
+    {
+        [Test]
+        public void Match_WhenMatchingExists_ThenMaximalMatching()
+        {
+            // given
+            var graph = new MultipartiteGraph<int, object, object>(2, new[] { new[] { 0, 2, 4, 6 },
+                                                                              new[] { 1, 3, 5, 7 } });
+            graph.AddEdgeBetween(graph[0], graph[3]);
+            graph.AddEdgeBetween(graph[0], graph[5]);
+            graph.AddEdgeBetween(graph[1], graph[2]);
+            graph.AddEdgeBetween(graph[3], graph[4]);
+            graph.AddEdgeBetween(graph[3], graph[6]);
+            graph.AddEdgeBetween(graph[6], graph[7]);
+
+            int[] matches = new int[] { 5, 2, 1, 4, 3, 0, 7, 6 };
+            var expected = Enumerable.Range(0, matches.Length).ToDictionary(i => graph[i], i => graph[matches[i]]);
+            // when
+            Dictionary<Vertex<int>, Vertex<int>> result = graph.Match();
+            // then
+            result.Should().ContainKeys(graph.Vertices);
+            result.Should().Contain(expected);
+        }
+
+        [Test]
+        public void Match_WhenVerticesOnlyInGroup0_ThenEmpty()
+        {
+            // given
+            var graph = new MultipartiteGraph<int, object, object>(2, new[] { new[] { 0, 1, 2, 3, 4 } });
+            // when
+            Dictionary<Vertex<int>, Vertex<int>> result = graph.Match();
+            // then
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Match_WhenVerticesOnlyInGroup1_ThenEmpty()
+        {
+            // given
+            var graph = new MultipartiteGraph<int, object, object>(
+                2, new[] { new int[] { }, new[] { 0, 1, 2, 3, 4 } });
+            // when
+            Dictionary<Vertex<int>, Vertex<int>> result = graph.Match();
+            // then
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Match_WhenTooManyGroups_ThenArgumentException()
+        {
+            // given
+            var graph = new MultipartiteGraph<int, object, object>(
+                3, new[] { new[] { 0 }, new[] { 1, 2 }, new[] { 3, 4 } });
+            // when
+            Action action = () => graph.Match();
+            // then
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ AlgoLib/Graphs.Algorithms/Matching.cs
@@ -0,0 +1,109 @@
+﻿// Hopcroft-Karp algorithm for matching in bipartite graph
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoLib.Graphs.Algorithms
+{
+    public static class Matching
+    {
+        /// <summary>Finds maximal matching in given bipartite graph.</summary>
+        /// <param name="graph">Bipartite graph</param>
+        /// <returns>Dictionary of matched vertices</returns>
+        public static Dictionary<Vertex<TVertexId>, Vertex<TVertexId>> Match<TVertexId, TVertexProperty, TEdgeProperty>(
+                this MultipartiteGraph<TVertexId, TVertexProperty, TEdgeProperty> graph)
+        {
+            var augmenter = new MatchAugmenter<TVertexId, TVertexProperty, TEdgeProperty>(graph);
+            bool wasAugmented = true;
+
+            while(wasAugmented)
+                wasAugmented = augmenter.augmentMatch();
+
+            return augmenter.matching;
+        }
+
+        private class MatchAugmenter<TVertexId, TVertexProperty, TEdgeProperty>
+        {
+            internal readonly Dictionary<Vertex<TVertexId>, Vertex<TVertexId>> matching =
+                new Dictionary<Vertex<TVertexId>, Vertex<TVertexId>>();
+
+            private const double infinity = double.PositiveInfinity;
+            private readonly MultipartiteGraph<TVertexId, TVertexProperty, TEdgeProperty> graph;
+
+            internal MatchAugmenter(MultipartiteGraph<TVertexId, TVertexProperty, TEdgeProperty> graph)
+            {
+                if(graph.GroupsCount != 2)
+                    throw new ArgumentException("Graph is not bipartite");
+
+                this.graph = graph;
+            }
+
+            internal bool augmentMatch()
+            {
+                var visited = new HashSet<Vertex<TVertexId>>();
+                var distances = graph.Vertices.ToDictionary(v => v, v => infinity);
+
+                bfs(distances);
+                return unmatchedVertices().Aggregate(false, (acc, v) => dfs(v, visited, distances) || acc);
+            }
+
+            private IEnumerable<Vertex<TVertexId>> unmatchedVertices() =>
+                graph.GetVerticesFromGroup(1).Where(v => !matching.ContainsKey(v));
+
+            private void bfs(Dictionary<Vertex<TVertexId>, double> distances)
+            {
+                var vertexDeque = new Queue<Vertex<TVertexId>>();
+
+                foreach(Vertex<TVertexId> vertex in unmatchedVertices())
+                {
+                    distances[vertex] = 0.0;
+                    vertexDeque.Enqueue(vertex);
+                }
+
+                while(vertexDeque.Count > 0)
+                {
+                    Vertex<TVertexId> vertex = vertexDeque.Dequeue();
+
+                    foreach(Vertex<TVertexId> neighbour in graph.GetNeighbours(vertex))
+                    {
+                        bool isMatched = matching.TryGetValue(neighbour, out Vertex<TVertexId> matched);
+
+                        if(isMatched && distances[matched] == infinity)
+                        {
+                            distances[matched] = distances[vertex] + 1;
+                            vertexDeque.Enqueue(matched);
+                        }
+                    }
+                }
+            }
+
+            private bool dfs(Vertex<TVertexId> vertex, HashSet<Vertex<TVertexId>> visited,
+                             Dictionary<Vertex<TVertexId>, double> distances)
+            {
+                visited.Add(vertex);
+
+                foreach(Vertex<TVertexId> neighbour in graph.GetNeighbours(vertex))
+                {
+                    bool isMatched = matching.TryGetValue(neighbour, out Vertex<TVertexId> matched);
+
+                    if(!isMatched)
+                    {
+                        matching[vertex] = neighbour;
+                        matching[neighbour] = vertex;
+                        return true;
+                    }
+
+                    if(!visited.Contains(matched) && distances[matched] == distances[vertex] + 1
+                            && dfs(matched, visited, distances))
+                    {
+                        matching[vertex] = neighbour;
+                        matching[neighbour] = vertex;
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
